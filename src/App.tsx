4a39681397a7e121ab0d683b9014a47261b0a567
victordelajarte <29466815+victordelajarte@users.noreply.github.com>
2026-02-07 18:45:41 +0100

    feat: show heart

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
-// import { createSignal } from 'solid-js';
+import { createSignal } from 'solid-js';
 import './App.css';
 import Heart from './components/heart';
 
+const goodEmojis = ['❤️', '💔', '💕', '💖', '💝', '😍', '🥰', '😘'] as const;
+const badEmojis = ['😬', '💩', '🤡'] as const;
+const allowedEmojis = [...goodEmojis, ...badEmojis] as const;
+interface EmojiInterface {
+  id: number;
+  x: number;
+  y: number;
+  emoji: (typeof allowedEmojis)[number];
+}
+
 function App() {
-  // const [count, setCount] = createSignal(0);
+  const [emojis, _setEmojis] = createSignal<EmojiInterface[]>([]);
 
   return (
     <>
-      {/* <h1>Will you be my Valentine?</h1> */}
+      <h1>Will you be my Valentine?</h1>
 
       <Heart />
+
+      {emojis().map((emoji) => (
+        <div
+          class="emoji"
+          style={{
+            left: `${emoji.x}px`,
+            top: `${emoji.y}px`,
+          }}
+        >
+          {emoji.emoji}
+        </div>
+      ))}
     </>
   );
 }
